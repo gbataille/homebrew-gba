8f90aadd4011d438f7afdc61c7c3c008e79934de
Grégory Bataille <gregory.bataille@gmail.com>
2016-01-06 08:54:09 +0100

    [Refactor] To pass the brew audit tests

--- Formula/githud.rb
+++ Formula/githud.rb
@@ -5,19 +5,18 @@ class Githud < Formula
 
   versionNumber = "1.2.0"
 
-  desc "A clean git HUD for your prompt"
+  desc "Clean git HUD for your prompt"
   homepage "https://github.com/gbataille/gitHUD"
-  url "https://hackage.haskell.org/package/gitHUD-#{versionNumber}/gitHUD-#{versionNumber}.tar.gz"
-  version versionNumber
+  url "https://hackage.haskell.org/package/gitHUD-#{version_number}/gitHUD-#{version_number}.tar.gz"
   sha256 "2af07420d64dad10cf3d0ab6198beed675006445d4c414078a63f2b553400791"
-
-  depends_on "ghc" => :build
-  depends_on "cabal-install" => :build
 
   bottle do
     root_url "https://raw.githubusercontent.com/gbataille/homebrew-gba/v#{versionNumber}/Bottles"
     sha256 "7597d0c4ba7a2600b5f36d398a14e478a90b2351e379a53cc1d4750e334c2edc" => :el_capitan
   end
+
+  depends_on "ghc" => :build
+  depends_on "cabal-install" => :build
 
   def install
     install_cabal_package
