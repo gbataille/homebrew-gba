6e4b9f552524054fd2d4381d04854c2622a1c837
Grégory Bataille <gregory.bataille@gmail.com>
2016-01-26 18:55:02 +0100

    v1.3.1

--- Formula/githud.rb
+++ Formula/githud.rb
@@ -3,16 +3,16 @@ require "language/haskell"
 class Githud < Formula
   include Language::Haskell::Cabal
 
-  version_number = "1.3.0"
+  version_number = "1.3.1"
 
   desc "Clean git HUD for your prompt"
   homepage "https://github.com/gbataille/gitHUD"
   url "https://hackage.haskell.org/package/gitHUD-#{version_number}/gitHUD-#{version_number}.tar.gz"
-  sha256 "b186502251e38f439a907eb54284ebb453b63003d91ec83c0c3b455f0da48568"
+  sha256 "d770a57be1e746ed39e1c34ae6464ac4fd66599503a8b4e92f1b8a69cf986ccb"
 
   bottle do
     root_url "https://raw.githubusercontent.com/gbataille/homebrew-gba/v#{version_number}/Bottles"
-    sha256 "7426ee57d2e72879a22f34fd6e1bf752d2b0255697cdd8f9448fd3ecbe2e669e" => :el_capitan
+    sha256 "68b53872acdec2ccfc3ca4a7c9f16aaa507757c07a04bfb46e58e6e2f8ef3158" => :el_capitan
   end
 
   depends_on "ghc" => :build
@@ -23,7 +23,7 @@ class Githud < Formula
   end
 
   def post_install
-    opoo "Some keys in the configuration file have changed. If you use the configuration file, please check it out"
+    opoo "Some proposed defaults in the configuration file have changed. If you have your own custom config, check out the changes in the new version"
   end
 
   test do
