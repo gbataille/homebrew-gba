a4b26b822e3487db9827592debe5325cbd0d5270
Grégory Bataille <gregory.bataille@gmail.com>
2016-08-15 18:55:21 +0200

    bottled 1.3.4

--- Formula/githud.rb
+++ Formula/githud.rb
@@ -10,10 +10,10 @@ class Githud < Formula
   url "https://hackage.haskell.org/package/gitHUD-#{version_number}/gitHUD-#{version_number}.tar.gz"
   sha256 "8c2273f0d2af162915291e3180f1910dfa76813be9cb7676c53fde112e8ad355"
 
-  # bottle do
-  #   root_url "https://github.com/gbataille/homebrew-gba/blob/master/Bottles/githud-#{version_number}.el_capitan.bottle.tar.gz?raw=true"
-  #   sha256 "8d54c8d761e9f0d0d158f127f1afd7e52942f5059de9342e54715251dec1f03d" => :el_capitan
-  # end
+  bottle do
+    root_url "https://github.com/gbataille/homebrew-gba/blob/master/Bottles/githud-#{version_number}.el_capitan.bottle.tar.gz?raw=true"
+    sha256 "b6d15734fafa52bfb6ea5db27b2412619fa723082036e523478bc0872d1aac6d" => :el_capitan
+  end
 
   depends_on "ghc" => :build
   depends_on "cabal-install" => :build
