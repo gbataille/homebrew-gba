fae2fe928e9fc9b48dbc1620cd340db3d4be1478
Grégory Bataille <gregory.bataille@gmail.com>
2017-02-07 19:16:40 +0100

    chore(): up to v1.3.6

--- Formula/githud.rb
+++ Formula/githud.rb
@@ -3,19 +3,17 @@ require "language/haskell"
 class Githud < Formula
   include Language::Haskell::Cabal
 
-  version_number = "1.3.5"
+  version_number = "1.3.6"
 
   desc "Clean git HUD for your prompt"
   homepage "https://github.com/gbataille/gitHUD"
   url "https://hackage.haskell.org/package/gitHUD-#{version_number}/gitHUD-#{version_number}.tar.gz"
-  sha256 "7956019a42632f8658ba9a6508943fd212e9796657252eedec53f48850a07009"
+  sha256 "a522924926b65d19601726fb5fde020c0523f0b30c95d6bb1e0ba0d751da8a49"
 
   bottle do
     root_url "https://github.com/gbataille/homebrew-gba/raw/master/Bottles/"
-    sha256 "1f9f0d1cae6a40ddb1c1eb20a6c1c65cd362340477950b11ed182cd6d6bebee8" => :el_capitan
-    sha256 "0564bbf7e12c723bfeacafe5e34831f42fa31744ef7631b40f55fd23d428a17d" => :sierra
+      sha256 "db2a0c664b12eef59bcfdb6aa0007e6cb16fe4b072425d0a857682f32c8e7122" => :sierra
   end
-
 
   depends_on "ghc" => :build
   depends_on "cabal-install" => :build
