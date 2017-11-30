6a569835a32d75977e180b0077aed2efdb39ae3f
Grégory Bataille <gregory.bataille@gmail.com>
2017-11-30 11:11:34 +0100

    fix(): bad root for bottles

--- Formula/githud.rb
+++ Formula/githud.rb
@@ -11,7 +11,7 @@ class Githud < Formula
   sha256 "dc38431b13d2dc4625987131c1389ed4ab67154990035c0c66f30d90d1a344af"
 
   bottle do
-    root_url "https://github.com/gbataille/homebrew-gba/raw/master/Bottles/"
+    root_url "https://github.com/gbataille/homebrew-gba/raw/master/Bottles"
     sha256 "5e4c97c4671c684dd8a9740f9b984bcc9ca9158a80d604e36ee60c68f30a2821" => :sierra
     sha256 "7488d011dcd0253123b5b8a161e013e00363ddc2c972b79756a08eb06e7d1b7f" => :high_sierra
   end
