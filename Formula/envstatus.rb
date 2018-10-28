7764dd788b8e75300792019f5510863c65e77ffd
Grégory Bataille <gregory.bataille@gmail.com>
2018-10-28 09:26:20 +0100

    chore(): revert unnecessary update - was a local config issue

--- Formula/envstatus.rb
+++ Formula/envstatus.rb
@@ -3,17 +3,17 @@ require "language/haskell"
 class Envstatus < Formula
   include Language::Haskell::Cabal
 
-  version_number = "1.0.1"
+  version_number = "1.0.0"
 
   desc "Display contextual information efficiently"
   homepage "https://github.com/gbataille/envstatus"
   url "https://hackage.haskell.org/package/envstatus-#{version_number}/envstatus-#{version_number}.tar.gz"
-  sha256 "9c23b019a2537c57966bfed17457ad4fa89b544457d486a54617981395cf6812"
+  sha256 "d288674798ae058563e5c6b628138035f0b81297f826ac15b952e8058bea9f1c"
 
-  # bottle do
-  #   root_url "https://github.com/gbataille/homebrew-gba/raw/master/Bottles"
-  #   sha256 "d805559b22cb87b338b937fd305b7ac855a83ea6721960a8317503cd04335e93" => :mojave
-  # end
+  bottle do
+    root_url "https://github.com/gbataille/homebrew-gba/raw/master/Bottles"
+    sha256 "d805559b22cb87b338b937fd305b7ac855a83ea6721960a8317503cd04335e93" => :mojave
+  end
 
 
   depends_on "ghc" => :build
