c9dcf43c16c2d9ba7798169c277febe830fae6b4
Grégory Bataille <gregory.bataille@gmail.com>
2019-08-09 07:29:25 +0200

    githud 3.0.1

--- Formula/githud.rb
+++ Formula/githud.rb
@@ -3,16 +3,16 @@ require "language/haskell"
 class Githud < Formula
   include Language::Haskell::Cabal
 
-  version_number = "3.0.0"
+  version_number = "3.0.1"
 
   desc "Clean git HUD for your prompt"
   homepage "https://github.com/gbataille/gitHUD"
   url "https://hackage.haskell.org/package/githud-#{version_number}/githud-#{version_number}.tar.gz"
-  sha256 "b98b2ea9f85f4d16b70290da1d2021a1821cc9b03bbd2b16b7da850e606927ff"
+  sha256 "e8122c0df1ecf2c69ebff5da71a8585fff9b8a97ede40ad06463452f8aea348a"
 
   bottle do
     root_url "https://github.com/gbataille/homebrew-gba/raw/master/Bottles"
-    sha256 "4a4a42ec207cd97c452d50d35c51b242713f55904473247b35242dba6e0a8eb7" => :mojave
+    sha256 "4375cb53b38cb95af69ae18366611aa48805c9a9edcfd7f570774b68ef2667bf" => :mojave
   end
 
 
