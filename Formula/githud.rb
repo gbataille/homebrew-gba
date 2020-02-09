8da11471e22cc2e3e015b79cd887804b9631716c
Grégory Bataille <gregory.bataille@gmail.com>
2020-02-09 07:12:01 +0100

    fix: compiles with ghc 8.8 which is the one brew distributes
    Fixes #1

--- Formula/githud.rb
+++ Formula/githud.rb
@@ -3,16 +3,16 @@ require "language/haskell"
 class Githud < Formula
   include Language::Haskell::Cabal
 
-  version_number = "3.2.0"
+  version_number = "3.2.1"
 
   desc "Clean git HUD for your prompt"
   homepage "https://github.com/gbataille/gitHUD"
   url "https://hackage.haskell.org/package/githud-#{version_number}/githud-#{version_number}.tar.gz"
-  sha256 "0a354f9731392ef1867d8c7fb0b1ae2351004680a673e09ad1baa02d140c028b"
+  sha256 "0b571b1f065d68aaeb92a4cc5d352bf56bb22f21bfcfb26bb4dc53b5f9b178ef"
 
   bottle do
     root_url "https://github.com/gbataille/homebrew-gba/raw/master/Bottles"
-    sha256 "0eed220cff58c66b18a92396ad398646473670499baacbeb19687b88bfabf6da" => :catalina
+    sha256 "94352ec6eb8bef42717fca95ef6b450435b63af86e08e456551bd846fdf52fcc" => :catalina
   end
 
   depends_on "ghc" => :build
